15d04a3f9995b5ba46fc0f6e44ff998a2fedb928
Xyrill Pythonista <xyrillariel@gmail.com>
2024-03-19 14:43:49 +0800

    modified secret key in setting.py and added the same secret key with additional $ sign to every $ sign in yml file, also in settings.py modified debug and yml debug=1

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
+#from django.core.management.utils import get_random_secret_key
+
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s$uyu-__5o)=$it+%g8(yn5=5jq9zerh+ynz-biwi7nl7ho0$%'
+# SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 

6ab5d80933a36fea34c080e1d480aa87fcfabd99
Xyrill Pythonista <xyrillariel@gmail.com>
2024-04-03 17:47:07 +0800

    added search functionality

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, SearchResultsListView
 
 urlpatterns = [
-    path('', BookListView.as_view(), name='book_list'),
-    path('<uuid:pk>', BookDetailView.as_view(), name='book_detail')  # new
+    path('', BookListView.as_view(), name='book_list'), # new, 1
+    path('<uuid:pk>', BookDetailView.as_view(), name='book_detail'), # new 2, for making book links to have its own ID
+    path('search', SearchResultsListView.as_view(), name='search_results'), # new 3
 
 ]
--- books/views.py
+++ books/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,         # used to load page for logged-in users only
     PermissionRequiredMixin,    # for custom permission
@@ -23,3 +25,15 @@ class BookDetailView(
     template_name = 'books/book_detail.html'
     login_url = 'account_login'
     permission_required = 'books.special_status'    # for custom permission
+
+
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )

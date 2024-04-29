85a6ef036ca3a7bb2f413b2e50c650838cbad7af
Xyrill Pythonista <xyrillariel@gmail.com>
2024-04-29 17:54:36 +0800

    changed app name

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['shielded-caverns-03832.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['pure-ridge-73757.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
